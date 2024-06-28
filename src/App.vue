8e30e23f2706641fcfcd40604ae9041012d0fb51
Matteo Conti <matt.ciao@gmail.com>
2024-06-28 17:00:58 +0200

    add filter link

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/components">Components</RouterLink>
+      <RouterLink to="/filter-area">Filter</RouterLink>
     </nav>
   </header>
   <!--  <FormStepper />-->
